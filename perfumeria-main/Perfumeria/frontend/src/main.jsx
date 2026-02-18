60a3b3a54fa3fcd62256072ab1170c729ba32ac7
Chapita2004 <157067134+Chapita2004@users.noreply.github.com>
2026-02-18 11:35:41 -0300

    Update main.jsx

--- perfumeria-main/Perfumeria/frontend/src/main.jsx
+++ perfumeria-main/Perfumeria/frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { CartProvider } from './context/CartContext'
-import { AuthProvider } from './context/authContext' // Agregamos esto
+import { AuthProvider } from './context/AuthContext' // Agregamos esto
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

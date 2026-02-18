b0e774452884090305574070cb0956c4b6ebd44a
Chapita2004 <157067134+Chapita2004@users.noreply.github.com>
2026-02-18 11:37:55 -0300

    Update AuthPage.jsx

--- perfumeria-main/Perfumeria/frontend/src/components/AuthPage.jsx
+++ perfumeria-main/Perfumeria/frontend/src/components/AuthPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/authContext';
+import { useAuth } from '../context/AuthContext';
 import { motion } from 'framer-motion';
 import { config } from '../api/config';
 

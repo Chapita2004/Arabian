31ea30887dd7ab6353ae2a7fe1a34af74976e749
Santiago Castro <castrogarciasantiagodaniel@gmail.com>
2026-02-18 11:32:06 -0300

    main

--- perfumeria-main/Perfumeria/frontend/src/main.jsx
+++ perfumeria-main/Perfumeria/frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { CartProvider } from './context/CartContext' 
-import { AuthProvider } from './context/AuthContext' // Agregamos esto
+import { CartProvider } from './context/CartContext'
+import { AuthProvider } from './context/authContext' // Agregamos esto
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
